fced9a15d4644b6e189e2d19d478caaa9f5354e9
wujovic <veljkovujovic185@gmail.com>
2022-02-04 19:52:46 +0100

    first commit, adding the main render objects, not finished yet.

---
+++ src/components/Heading.tsx
@@ -0,0 +1,8 @@
+export default function Heading() {
+    return (
+        <div className='heading'>
+            <h1>Border Radius Previewer & Generator</h1>
+            <p>Give us your values, and we will make your life easier!</p>
+        </div>
+    )
+}
---
+++ src/components/PreviewBox.tsx
@@ -0,0 +1,27 @@
+export default function PreviewBox() {
+    return (
+        <div className='box-container'>
+
+            <div className='left-boxes'>
+
+                <div id="left-up-box">up</div>
+                <div id="left-down-box">dwn</div>
+
+            </div>
+
+            <div className='main-box'>
+                <div className='inner-main-box'>
+
+                </div>
+            </div>
+
+            <div className='right-boxes'>
+
+                <div id="right-up-box">up</div>
+                <div id="right-down-box">dwn</div>
+
+            </div>
+
+        </div>
+    )
+}
